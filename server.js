3c3939ec758ab2d560b368f6c2478275493e671d
kamos1 <keji.amos@gmail.com>
2017-05-24 09:16:00 -0600

    create express server

---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const environment = process.env.NODE_ENV || 'development';
+// const configuration = require('./knexfile')[environment];
+// const database = require('knex')(configuration)
+
+app.use(bodyParser.json());
+
+app.get('/api/v1/users', (request, response) => {
+  database('users').select()
+    .then(users => {
+      response.status(200).json(users);
+    })
+    .catch(error => {
+      console.error('error:', error);
+    });
+});
+
+app.get('/api/v1/wins', (request, response) => {
+  database('wins').select()
+    .then(wins => {
+      response.status(200).json(wins)
+    })
+    .catch(error => {
+      console.error('error', error);
+    })
+})
+
+app.get('/api/v1/losses', (request, response) => {
+  database('losses').select()
+    .then(losses => {
+      response.status(200).json(losses)
+    })
+    .catch(error => {
+      console.error('error', error);
+    })
+})
+
+app.listen(3000, () => {
+  console.log('Server is listening on port 3000');
+})
