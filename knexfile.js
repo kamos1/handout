5084e8bce4e35c63936a699791888039f8963ab8
kamos1 <keji.amos@gmail.com>
2017-05-24 16:29:30 -0600

    update knexfile

--- knexfile.js
+++ knexfile.js
@@ -33,7 +33,7 @@ module.exports = {
   production: {
     client: 'postgresql',
     connection: {
-      database: 'my_db',
+      database: 'postgres://alwgjshlbfavea:671c0675cac59368e2c5a9c3fd864d5b71276aebf9d5a9639ef0a12942f5a08f@ec2-54-197-232-155.compute-1.amazonaws.com:5432/d9lpul1fp5qipj',
       user:     'username',
       password: 'password'
     },
