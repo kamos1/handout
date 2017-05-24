0a2d8e4ce64b141743d9e1ff5107bf32a9eaecc7
kamos1 <keji.amos@gmail.com>
2017-05-24 17:00:50 -0600

    redo knexfile

--- knexfile.js
+++ knexfile.js
@@ -14,26 +14,10 @@ module.exports = {
     useNullAsDefault: true
   },
 
-  // staging: {
-  //   client: 'postgresql',
-  //   connection: {
-  //     database: 'my_db',
-  //     user:     'username',
-  //     password: 'password'
-  //   },
-  //   pool: {
-  //     min: 2,
-  //     max: 10
-  //   },
-  //   migrations: {
-  //     tableName: 'knex_migrations'
-  //   }
-  // },
-  //
-  production: {
+  staging: {
     client: 'postgresql',
     connection: {
-      database: 'postgres://alwgjshlbfavea:671c0675cac59368e2c5a9c3fd864d5b71276aebf9d5a9639ef0a12942f5a08f@ec2-54-197-232-155.compute-1.amazonaws.com:5432/d9lpul1fp5qipj',
+      database: 'my_db',
       user:     'username',
       password: 'password'
     },
@@ -42,7 +26,21 @@ module.exports = {
       max: 10
     },
     migrations: {
-      directory: './db/migrations'
+      tableName: 'knex_migrations'
+    }
+  },
+  
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'postgres://alwgjshlbfavea:671c0675cac59368e2c5a9c3fd864d5b71276aebf9d5a9639ef0a12942f5a08f@ec2-54-197-232-155.compute-1.amazonaws.com:5432/d9lpul1fp5qipj',
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
     },
     ssl: true
   }
