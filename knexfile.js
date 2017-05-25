2d49e75990008ced68525c71b7f75b82c083724f
kamos1 <keji.amos@gmail.com>
2017-05-24 22:34:38 -0600

    update knexfile

--- knexfile.js
+++ knexfile.js
@@ -15,7 +15,7 @@ module.exports = {
   },
 
   staging: {
-    client: 'postgresql',
+    client: 'pg',
     connection: {
       database: 'my_db',
       user:     'username',
@@ -32,7 +32,7 @@ module.exports = {
 
   production: {
     client: 'pg',
-    connection: process.env.DATABASE_URL + '?ssl=true',
+    connection: process.env.DATABASE_URL,
     migrations: {
       directory: './db/migrations'
     },
