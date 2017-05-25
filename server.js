0873c0670d1e8fc5813bd60695bd8a7818c08958
kamos1 <keji.amos@gmail.com>
2017-05-24 22:14:36 -0600

    server fix?

--- server.js
+++ server.js
@@ -10,7 +10,10 @@ app.use(bodyParser.json());
 
 app.set('port', (process.env.PORT || 3000));
 //verify app is working
-app.get('/', (request, response) => response.send('It works!'));
+// app.get('/', (request, response) => response.send('It works!'));
+app.get('/', function(req, res){
+  res.send('It works!');
+});
 
 // app.post('/api/v1/users', ((request, response) => {
 //     console.log(request.body);
@@ -21,36 +24,36 @@ app.get('/', (request, response) => response.send('It works!'));
 //   })
 // )
 
-app.get('/api/v1/users', (request, response) => {
-  console.log(request.body);
-  database('users').select()
-    .then(users => {
-      response.status(200).json(users);
-    })
-    .catch(error => {
-      console.error('error:', error);
-    });
-});
-
-app.get('/api/v1/wins', (request, response) => {
-  database('wins').select()
-    .then(wins => {
-      response.status(200).json(wins)
-    })
-    .catch(error => {
-      console.error('error', error);
-    })
-})
-
-app.get('/api/v1/losses', (request, response) => {
-  database('losses').select()
-    .then(losses => {
-      response.status(200).json(losses)
-    })
-    .catch(error => {
-      console.error('error', error);
-    })
-})
+// app.get('/api/v1/users', (request, response) => {
+//   console.log(request.body);
+//   database('users').select()
+//     .then(users => {
+//       response.status(200).json(users);
+//     })
+//     .catch(error => {
+//       console.error('error:', error);
+//     });
+// });
+//
+// app.get('/api/v1/wins', (request, response) => {
+//   database('wins').select()
+//     .then(wins => {
+//       response.status(200).json(wins)
+//     })
+//     .catch(error => {
+//       console.error('error', error);
+//     })
+// })
+//
+// app.get('/api/v1/losses', (request, response) => {
+//   database('losses').select()
+//     .then(losses => {
+//       response.status(200).json(losses)
+//     })
+//     .catch(error => {
+//       console.error('error', error);
+//     })
+// })
 
 app.listen(app.get('port'), () => {
   console.log(`Server is running on ${app.get('port')}.`)
