a1bddfc225e126f5dd5997858ec9994d612bb917
kamos1 <keji.amos@gmail.com>
2017-05-30 11:30:18 -0600

    remove switch statements

--- server.js
+++ server.js
@@ -44,43 +44,54 @@ app.post('/add', (request, response) => {
     text: `${user} recieved a ${type}`
   };
 
-  switch (type) {
-    case 'win':
-      User.findOrCreate({ userID: user, username: username })
+  type === "win" ? outcome_type_id = 1 : outcome_type_id = 2;
+
+  User.findOrCreate({ userID: user, username: username })
         .then((user) => {
           Outcome.create({user_id: user.id, outcome_types_id: 1})
         })
         .then(() => response.status(200).send(body))
         .catch((error) => response.status(500).send(error))
-    case 'loss':
-      User.findOrCreate({ userID: user, username: username })
-        .then((user) => {
-          Outcome.create({user_id: user.id, outcome_types_id: 2})
-        })
-        .then(() => response.status(200).send(body))
-        .catch((error) => response.status(500).send(error))
-  };
+
+  // switch (type) {
+  //   case 'win':
+  //     User.findOrCreate({ userID: user, username: username })
+  //       .then((user) => {
+  //         Outcome.create({user_id: user.id, outcome_types_id: 1})
+  //       })
+  //       .then(() => response.status(200).send(body))
+  //       .catch((error) => response.status(500).send(error))
+  //   case 'loss':
+  //     User.findOrCreate({ userID: user, username: username })
+  //       .then((user) => {
+  //         Outcome.create({user_id: user.id, outcome_types_id: 2})
+  //       })
+  //       .then(() => response.status(200).send(body))
+  //       .catch((error) => response.status(500).send(error))
+  // };
 });
 
 app.post('/check', (request, response) => {
   const text = request.body.text.split(' ');
   const type = text[0].replace(/['",]+/g, '');
   const user = text[1].replace(/['",]+/g, '');
+  const userInfo = user.split('|')
+  const username = userInfo[1].replace(/['",>]+/g, '');
+  let outcome_type_id;
 
-  switch (type) {
-    case 'win':
-      User.findOne({ userID: user})
+  if (type === 'win') {
+      User.findOne({ userID: user, username: username })
       .then((user) => Outcome.findAll({user_id: user.id, outcome_types_id: 1}))
       .then((outcomes) => response.status(200)
         .send({text: `You have ${outcomes.length} wins` }))
       .catch((error) => response.status(500).send(error))
-    case 'loss':
+    } else {
       User.findOne({ userID: user})
       .then((user) => Outcome.findAll({user_id: user.id, outcome_types_id: 2}))
       .then((outcomes) => response.status(200)
         .send({text: `You have ${outcomes.length} losses` }))
       .catch((error) => response.status(500).send(error))
-  }
+    }
 });
 
 app.listen(app.get('port'), () => {
