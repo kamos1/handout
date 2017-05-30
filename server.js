6b6c15a28a38e0a3cad7cf0235de088025700b7b
kamos1 <keji.amos@gmail.com>
2017-05-29 18:23:23 -0600

    add testing add second post request

--- server.js
+++ server.js
@@ -4,23 +4,25 @@ const bodyParser = require('body-parser');
 
 const environment = process.env.NODE_ENV || 'development';
 const configuration = require('./knexfile')[environment];
-const database = require('knex')(configuration)
-const bookshelf = require('bookshelf')(database)
+const database = require('knex')(configuration);
+const bookshelf = require('bookshelf')(database);
 const ModelBase = require('bookshelf-modelbase')(bookshelf);
 
 const User = ModelBase.extend({
   tableName: 'users',
   outcomes: () => this.hasMany(Outcome),
-})
+});
+
 const Outcome = ModelBase.extend({
   tableName: 'outcomes',
   users: () => this.belongsTo(User),
   outcome_types: () => this.belongsTo(Outcome_Type)
-})
+});
+
 const Outcome_Type = ModelBase.extend({
   tableName: 'outcome_types',
   outcomes: () => this.hasMany(Outcome)
-})
+});
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -30,32 +32,50 @@ app.set('port', (process.env.PORT || 3000));
 app.get('/', (request, response) => response.send('It works!'));
 
 app.post('/add', (request, response) => {
-    const text = request.body.text.split(' ');
-    const type = text[0]
-    const user_id = text[1]
-    var fixType = type.replace(/win',/i, 'win');
-    let outcome_type_id;
+  const text = request.body.text.split(' ');
+  const type = text[0];
+  const user = text[1];
+  const fixUser = user.replace(/['",]+/g, '');
+  const fixType = type.replace(/['",]+/g, '');
+  let outcome_type_id;
 
-    fixType === "win" ? outcome_type_id = 1 : outcome_type_id = 2
+  fixType === "win" ? outcome_type_id = 1 : outcome_type_id = 2;
 
-    const body = {
-      response_type: "in_channel",
-      text: `${user_id} recieved a ${type}`
-    };
+  const body = {
+    response_type: "in_channel",
+    text: `${fixUser} recieved a ${fixType}`
+  };
 
-    User.findOrCreate({ userID: user_id })
-      .then((user) => {
-        Outcome.create({user_id: user.id, outcome_types_id: outcome_type_id})
-      })
-      .then(() => response.status(200).send(body))
-      .catch((error) => response.status(500).send(error))
-})
+  User.findOrCreate({ userID: fixUser })
+    .then((user) => {
+      Outcome.create({user_id: user.id, outcome_types_id: outcome_type_id})
+    })
+    .then(() => response.status(201).send(body))
+    .catch((error) => response.status(500).send(error))
+  });
 
 app.post('/check', (request, response) => {
-  console.log(request.body);
-});
+  const text = request.body.text.split(' ');
+  const type = text[0].replace(/['",]+/g, '');
+  const user = text[1].replace(/['",]+/g, '');
 
+  if (type === 'win') {
+    User.findOne({ userID: user})
+    .then((user) => Outcome.findAll({user_id: user.id, outcome_types_id: 1}))
+    .then((outcomes) => response.status(200)
+      .send({response_type: "in_channel", text: `You have ${outcomes.length} wins` }))
+    .catch((error) => response.status(500).send(error))
+  } else {
+    User.findOne({ userID: user})
+    .then((user) => Outcome.findAll({user_id: user.id, outcome_types_id: 2}))
+    .then((outcomes) => response.status(200)
+      .send({response_type: "in_channel", text: `You have ${outcomes.length} losses` }))
+    .catch((error) => response.status(500).send(error))
+  }
+});
 
 app.listen(app.get('port'), () => {
   console.log(`Server is running on ${app.get('port')}.`)
-})
+});
+
+module.exports = app
