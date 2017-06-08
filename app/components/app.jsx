49e1e274dd7f4c78869dbc71fa512761b07359bd
kamos1 <keji.amos@gmail.com>
2017-06-08 07:06:08 -0600

    update app.jsx

--- app/components/app.jsx
+++ app/components/app.jsx
@@ -5,7 +5,7 @@ export default class App extends Component {
   render() {
     return (
       <div className='main'>
-        woiejfwoiafjaoiejf
+        Keji did it again!
       </div>
     );
   }
