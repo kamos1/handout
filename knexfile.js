f83b015e512e82c2d6c0ffd96c52f8d6610dfcd1
kamos1 <keji.amos@gmail.com>
2017-07-05 10:26:16 -0600

    add circleci and auto deployment

--- knexfile.js
+++ knexfile.js
@@ -16,7 +16,7 @@ module.exports = {
 
   test: {
     client: 'pg',
-    connection: 'postgres://localhost/slack_test',
+    connection: process.env.DATABASE_URL || 'postgres://localhost/slack_test',
     migrations: {
       directory: './db/migrations'
     },
